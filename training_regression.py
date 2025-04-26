5ade26d1594a2c95b22bb4581a66ffa9ec1a0c4b
M-i-r-c-o <mirco.11@hotmail.ch>
2025-04-26 03:06:27 +0200

    Update training_regression.py

--- training_regression.py
+++ training_regression.py
@@ -58,8 +58,8 @@ if __name__ == '__main__':
     print('\nFinished training !\n')
     sys.stdout.flush()
 
-    with open('training_regression_mse_losses_21_04.txt', 'w') as f: 
+    with open('training_regression_mse_losses_26_04.txt', 'w') as f: 
         f.write(json.dumps(all_mse_losses, sort_keys=True, indent=2))
 
-    with open('training_regression_nmse_losses_21_04.txt', 'w') as f:
+    with open('training_regression_nmse_losses_26_04.txt', 'w') as f:
         f.write(json.dumps(all_nmse_losses, sort_keys=True, indent=2))
