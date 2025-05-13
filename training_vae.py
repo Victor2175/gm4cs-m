7573a58966073504083fe8ea8df4cbe1e44a504f
M-i-r-c-o <mirco.11@hotmail.ch>
2025-05-13 17:48:03 +0200

    Updating vae training

--- training_vae.py
+++ training_vae.py
@@ -30,31 +30,44 @@ if __name__ == '__main__':
     sys.stdout.flush()
 
     # Specify parameters
-    lr = 1e-3
-    batch_size = 16
     epochs = 30
+    lrs = [1e-3, 1.5e-3, 2e-3]
+    hidden_dims = [500, 1000, 1500]
+    latent_dims = [100, 200, 300]
+    configurations = len(lrs)*len(hidden_dims)*len(latent_dims)
+
+    batch_size = 16
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f'Using device: {device}')
     sys.stdout.flush()
 
-    model = VAE(input_dim=44472, hidden_dim=500, latent_dim=100).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    
     ith = 1
 
     print('Begin cross validation...\n')
     sys.stdout.flush()
 
-    losses = VAE_LOOCV(cope_data, union_nan_mask, model, epochs, batch_size, lr, verbose=True)
+    all_mse_losses = {}
+    all_nmse_losses = {}
+    for lr in lrs:
+        for hidden_dim in hidden_dims:
+            for latent_dim in latent_dims:
+                model = VAE(input_dim=44472, hidden_dim=hidden_dim, latent_dim=latent_dim).to(device)
+                mse_losses, nmse_losses = VAE_LOOCV(cope_data, union_nan_mask, model, epochs, batch_size, lr)
 
-    print(f'Finished !')
-    sys.stdout.flush()
+                all_mse_losses[f'vae_{lr}_{epochs}'] = mse_losses
+                all_nmse_losses[f'vae_{lr}_{epochs}'] = nmse_losses
+                
+                print(f'Finished configuration {ith}/{configurations}')
+                sys.stdout.flush()
 
-    ith += 1
+                ith += 1
 
     print('\nFinished training !\n')
     sys.stdout.flush()
 
-    with open('training_vae_losses_23_04.txt', 'w') as f: 
-        f.write(json.dumps(losses, indent=2))
+    with open('training_vae_mse_losses_05_05.txt', 'w') as f: 
+        f.write(json.dumps(all_mse_losses, sort_keys=True, indent=2))
+
+    with open('training_vae_nmse_losses_05_05.txt', 'w') as f:
+        f.write(json.dumps(all_nmse_losses, sort_keys=True, indent=2))
